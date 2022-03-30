1f91f575f19597f83a0bd6013be0800e042040cc
richard <richard.klem@seznam.cz>
2022-03-30 23:24:47 +0200

    RRG to NFA without finite states at the moment.

--- flp21-fun/src/Lib.hs
+++ flp21-fun/src/Lib.hs
@@ -8,10 +8,13 @@ module Lib
   )
 where
 
-import Data.List (intersperse, nub)
+import Data.List (intersperse, nub, elemIndex, intercalate)
+import Data.Map (fromList, lookup)
+import Prelude hiding (lookup)
+import Data.Maybe (fromJust)
 import System.Environment
-import Utils
 import GHC.Conc (pseq)
+import Utils
 
 -- data RLG = RLG {  nonterminals :: [String], terminals :: [String], startSymbol :: Char, rules :: [(Char, String)] }
 type Nonterminals = [String]
@@ -41,7 +44,7 @@ data NFA = NFA
 
 getInput :: [FilePath] -> IO [String]
 getInput [] = lines <$> getContents
-getInput [x] = lines <$> readFile x  
+getInput [x] = lines <$> readFile x
 getInput _ = error "Invalid arguments."
 
 newParse :: [String] -> (Int, [String])
@@ -65,7 +68,7 @@ showRLG a =
 
 showRRG :: RRG -> String
 showRRG a = "Right Regular Grammar:\n" ++ showRLG a
-    
+
 showNFA :: NFA -> String
 showNFA a =
   "Non-deterministic Finite Automaton:\n" ++ concat
@@ -73,11 +76,10 @@ showNFA a =
         [ [concat $ intersperse "," (map show (states a)) ++ ["\n"]],
           [inputAlphabet a ++ "\n"],
           [show (initialState a) ++ "\n"],
-          [show (finalStates a) ++ "\n"],
-          [unlines $ map (\(q, c, p) -> show q ++ [c] ++ show p) (transitionFunction a)] -- Upravit na novy type Rules
+          [concat $ intersperse "," (map show (finalStates a)) ++ ["\n"]],
+          [unlines $ (map (\(q, c, p) -> intercalate "," [show q, [c], show p]) (transitionFunction a))]
         ]
-    ) 
-
+    )
 
 checkIfValidRule :: String -> Rule
 checkIfValidRule (left : y : z : right) =
@@ -88,11 +90,9 @@ checkIfValidRule (left : y : z : right) =
     (
       ((upperCount right == 1) && ((last right `elem` ['A'..'Z']) && length right > 1)) ||
       ((upperCount right == 0) && ((last right `elem` ['a'..'z']) || (last right == '#')))
-    ) 
+    )
   then ([left], right) else error ("Spatna syntaxe pravidla: " ++ left:y:z:right)
 checkIfValidRule rule = error ("Spatna syntaxe pravidla: " ++ rule)
-
-
 
 -- AA->Aa  //INVALID
 -- A->Aa   //INVALID
@@ -106,21 +106,24 @@ checkIfValidRule rule = error ("Spatna syntaxe pravidla: " ++ rule)
 
 -- Convert rule from RLG to type 3 grammar (RG).
 -- Zde se jiz muzu psolehnout na korektni tvar pravidel.
-convertRule :: Rule -> Rules
+convertRuleRRG :: Rule -> Rules
 --convertRule r = if ((last (snd r) `elem` ['A' .. 'Z']) && length (snd r) == 2 ) || snd r == "#" then [r] else [("","")]
-convertRule r = [r] -- TODO 
+convertRuleRRG r = [r] -- TODO
 
---convertToRRG :: RLG -> RRG
-convertToRRG a = do -- TODO 
-  return a
-  
---convertToNFA :: RRG -> NFA
-convertToNFA a = do -- TODO 
-   return NFA { states = [0,0],
-                     inputAlphabet = "abc",
-                     initialState = 1,
-                     finalStates = [42],
-                     transitionFunction = [(0, 'a', 42), (0, 'c', 0)] } 
+-- getTransitionFunction :: Rules -> TransitionFunction
+getTransitionFunction rs dict = map (\(l, r) -> if r == "#" then (fromJust(lookup l dict), '#', fromJust(lookup l dict)) else (fromJust(lookup l dict), head r, fromJust(lookup (tail r) dict))) rs
+
+-- convertToRRG :: RLG -> RRG
+convertToRRG = return -- TODO
+
+--convertToNFA :: RRG -> NFA -- TODO
+convertToNFA rrg = do
+  let dict = fromList (zip (nonterminals rrg) [0..length (nonterminals rrg) - 1])
+  return NFA { states = [0.. length (nonterminals rrg) - 1],--(map (\nt -> fromJust (elemIndex nt (map head (nonterminals rrg)))) (map head (nonterminals rrg))),
+               inputAlphabet = concat (terminals rrg),
+               initialState = fromJust (elemIndex (startSymbol rrg) (map head (nonterminals rrg))),
+               finalStates = [0.. length (nonterminals rrg) - 1],
+               transitionFunction = getTransitionFunction (rules rrg) dict}
 
 {-___ MAIN ___-}
 rlg2nfa :: IO ()
@@ -128,15 +131,15 @@ rlg2nfa :: IO ()
 rlg2nfa = do
     args <- getArgs
     let (variant, fileName) = newParse args
-    input <- getInput fileName 
-    let n = length input 
-   
-    case variant of 
+    input <- getInput fileName
+    let n = length input  --n `pseq` (something) je hack na vyckani z evaluaci stdin az do doby kdy je poslan eof a ne driv.
+
+    case variant of
       0 -> getRLG input >>= (n `pseq` (putStr . showRLG))
       1 -> getRLG input >>= convertToRRG >>= (n `pseq` (putStr . showRRG))
       2 -> getRLG input >>= convertToRRG >>= convertToNFA >>= (n `pseq` (putStr . showNFA))
       _ -> error "Neplatne argumenty."
-    
+
 
 getRLG input = do
   let nonterminalsString : terminalsString : startSymbolString : rulesString = input
@@ -144,10 +147,11 @@ getRLG input = do
   let terminals = map skipSpaces (nub (wordsWhen (== ',') terminalsString))
   let startSymbol = if length (skipSpaces startSymbolString) <= 1 then head $ skipSpaces startSymbolString else error "Startovaci symbol musi byt jeden znak na samostatnem radku."
 --  let rules = filter (/= ("", "")) (map checkIfValidRule (nub $ filter (not . null) rulesString))
-  let rules = concatMap (convertRule . checkIfValidRule . skipSpaces) (nub $ filter (not . null) rulesString)
+  let rules = concatMap (convertRuleRRG . checkIfValidRule . skipSpaces) (nub $ filter (not . null) rulesString)
   let rlg = RLG nonterminals terminals startSymbol rules
 
   return rlg
+
 
 --
 -- parse :: [String] -> IO ()
